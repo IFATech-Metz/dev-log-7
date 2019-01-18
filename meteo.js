97c09e244231e1acc58e51c9bee64fbed546f52a
valentinmaas <ValentinMaas9@gmail.com>
2019-01-18 13:47:36 +0100

    Version 18/01/2019

--- meteo.js
+++ meteo.js
@@ -6,7 +6,8 @@ var xhr = new XMLHttpRequest();
 var base_url = "http://api.openweathermap.org/data/2.5/weather";
 var city = "Metz";
 var units = "metric";
-var appid = "348e43383864ecfba8b0827cc402f3ff";
+var appid = "dd8984d9d3055c049aa688cc2dc715b7";
+var urlforecast = "http://api.openweathermap.org/data/2.5/forecast";
 
 function get_url() {
     return base_url + "?"
@@ -15,23 +16,44 @@ function get_url() {
         + "appid=" + appid;
 }
 
+function get_urlforecast(){
+    return urlforecast + "?"
+    + "q=" + city + "&" 
+    + "units=" + units + "&"
+    + "appid=" + appid;
+}
+
 function init_page() {
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("url").innerHTML = get_url();
+            document.getElementById("url").innerHTML = get_urlforecast();
 
             var response = JSON.parse(this.responseText);
             var temperature = response.main.temp;
 
             var icon = response.weather[0].icon;
             var src = "http://openweathermap.org/img/w/" + icon + ".png";
+            var response = JSON.parse(this.responseText);
+            var tempforecast = response.list.main.temp;
+            var tempminforecast = response.list.main.temp_min;
+            var tempmaxforecast = response.list.main.temp_max;
+            var iconforecast = response.list.weather.icon;
 
+            var src = "http://openweathermap.org/img/w/" + icon + ".png";
+
+            document.getElementById("iconforecast").src = src;
+            document.getElementById("tempforecast").innerhtml = tempforecast;
+            document.getElementById("tempmin").innerhtml = tempminforecast;
+            document.getElementById("tempmax").innerhtml = tempmaxforecast;
             document.getElementById("meteo").innerHTML = temperature;
             document.getElementById("icon").src = src;
         }
     };
     
     xhr.open("GET", get_url(), true);
+    xhr.send();
+    xhr.open("GET", get_urlforecast(), true);
     xhr.send();
 }
 
@@ -63,4 +85,35 @@ function get_temperature() {
     
     xhr.open("GET", get_url(), true);
     xhr.send();
+}
+
+function get_forecast(){
+    city = document.getElementById("ville").value;
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("url").innerHTML = get_urlforecast();
+
+            if(document.getElementById("url_visibility").checked) {
+                document.getElementById("url").style.display = "block";
+            }
+            else {
+                document.getElementById("url").style.display = "none";
+            }
+
+            var response = JSON.parse(this.responseText);
+            var tempforecast = response.list.main.temp;
+            var tempminforecast = response.list.main.temp_min;
+            var tempmaxforecast = response.list.main.temp_max;
+            var iconforecast = response.list.weather.icon;
+
+            var src = "http://openweathermap.org/img/w/" + icon + ".png";
+
+            document.getElementById("iconforecast").src = src;
+            document.getElementById("tempforecast").innerhtml = tempforecast;
+            document.getElementById("tempmin").innerhtml = tempminforecast;
+            document.getElementById("tempmax").innerhtml = tempmaxforecast;
+        }
+        xhr.open("GET", get_urlforecast(), true);
+        xhr.send();
+    }    
 }
